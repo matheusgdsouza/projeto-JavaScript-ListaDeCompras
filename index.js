4a0aa272f4ede1c3c3a56c7465cf807461e96dd4
Matheus de Souza <matheusgds3011@gmail.com>
2026-03-04 15:30:01 -0300

    Criando novo item da lista

--- index.js
+++ index.js
@@ -2,15 +2,32 @@
 const inputItem = document.getElementById('input-item');
 const botaoAdicionar = document.getElementById('adicionar-item');
 const listaDeCompras = document.getElementById('lista-de-compras');
+let contador = 0;
 
 // Adicionando evento de clique ao botão "Adicionar"
 botaoAdicionar.addEventListener('click', function(evento) {
     evento.preventDefault();
-    const itemLista = inputItem.value;
+    let itemLista = inputItem.value;
 // Condicional para verificar se o campo de entrada está vazio
     if (itemLista === "") {
         alert("Por favor, insira um item para adicionar à lista de compras.");
         return;
     } 
-    
+
+//Criando elementos e adicionando item a lista
+    const novoItemLista = document.createElement("li");
+    const containerLista = document.createElement("div");
+    containerLista.classList.add("lista-item-container");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = `checkbox-${contador}`;
+    const textoItem = document.createElement("p");
+    textoItem.textContent = itemLista;
+    containerLista.appendChild(checkbox);
+    containerLista.appendChild(textoItem);
+    novoItemLista.appendChild(containerLista);
+
+
+// Limpando o campo de entrada após adicionar o item à lista
+    inputItem.value = "";
     });
