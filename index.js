12693dc8620684657f71d289bb9bad005d82616e
Matheus de Souza <matheusgds3011@gmail.com>
2026-03-04 14:37:05 -0300

    Adicionando condicional de controle

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+// Pegando valores importantes no DOM
 const inputItem = document.getElementById('input-item');
 const botaoAdicionar = document.getElementById('adicionar-item');
 const listaDeCompras = document.getElementById('lista-de-compras');
 
-botaoAdicionar.addEventListener('click', function() {
-  const itemLista = inputItem.value;
+// Adicionando evento de clique ao botão "Adicionar"
+botaoAdicionar.addEventListener('click', function(evento) {
+    evento.preventDefault();
+    const itemLista = inputItem.value;
+// Condicional para verificar se o campo de entrada está vazio
+    if (itemLista === "") {
+        alert("Por favor, insira um item para adicionar à lista de compras.");
+        return;
+    } 
+    
     });
