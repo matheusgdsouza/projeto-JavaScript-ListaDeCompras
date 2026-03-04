cc1a40fdf360871e704117357bd116e807d4905e
Matheus de Souza <matheusgds3011@gmail.com>
2026-03-03 21:25:48 -0300

    Guardando informação fornecida pelo usuario

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Salve!");
+const inputItem = document.getElementById('input-item');
+const botaoAdicionar = document.getElementById('adicionar-item');
+const listaDeCompras = document.getElementById('lista-de-compras');
+
+botaoAdicionar.addEventListener('click', function() {
+  const itemLista = inputItem.value;
+    });
